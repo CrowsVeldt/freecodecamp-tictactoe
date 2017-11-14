cd118c76e28ac443e8a2aca26eb103282c681a2b
CrowsVeldt <zechy.mahler@gmail.com>
2017-11-14 10:28:09 +0200

    add game, board, squares

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-class App extends Component {
-  render() {
+class Square extends Component {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <div className='game-square'>X</div>
+    )
   }
 }
 
-export default App;
+class Board extends Component {
+  render () {
+    return (
+      <div className='game-board'>
+        <Square key='0' />
+        <Square key='1' />
+        <Square key='2' />
+        <Square key='3' />
+        <Square key='4' />
+        <Square key='5' />
+        <Square key='6' />
+        <Square key='7' />
+        <Square key='8' />
+      </div>
+    )
+  }
+}
+
+class Game extends Component {
+  render () {
+    return (
+      <div className='game'>
+        <h1 className='game-title'>Tic Tac Toe</h1>
+        <Board />
+      </div>
+    )
+  }
+}
+
+export default Game
